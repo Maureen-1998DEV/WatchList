819f633f880b47be81a6f8ecb340b87f0af3521f
Maureen Ougo <maureen.ougo@student.moringaschool.com>
2021-09-08 10:42:47 +0300

    pushing the folder to github

---
+++ app/config.py
@@ -0,0 +1,19 @@
+class Config:
+    '''
+    General configuration parent class
+    '''
+    pass
+class ProdConfi(Config):
+    '''
+    Production configuration child class
+    Arg:
+    Config:The parent configuration class with General configuration settings
+    '''
+    pass
+class DevConfig(Config):
+    '''
+    Development configuration child class
+    Args:
+    Config:The parent configuration class with General configuration settings
+    '''
+    Debug = True
